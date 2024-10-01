96cbe3eca19a044c91c77436a4f9bf21744426bc
imhasandl <imhasandl04@gmail.com>
2024-10-01 19:52:03 +0500

    http URLs

--- http/http.go
+++ http/http.go
@@ -1,43 +1,43 @@
-// Assignment
-// Take a look at the getItemData function that I've provided in http.go. It retrieves information about items from Fantasy Quest's servers via HTTP as a slice of bytes []byte.
+// // Assignment
+// // Take a look at the getItemData function that I've provided in http.go. It retrieves information about items from Fantasy Quest's servers via HTTP as a slice of bytes []byte.
 
-// In main.go do the following:
+// // In main.go do the following:
 
-// Convert the slice of bytes to a string with the built-in string() type conversion.
-// Print the string representation of the bytes to the console.
-// It should look like a big ugly string of text.
-
-
-package main
-
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"log"
-)
-
-func getItemData() ([]byte, error) {
-	res, err := http.Get("https://api.boot.dev/v1/courses_rest_api/learn-http/items")
-	if err != nil {
-		return nil, fmt.Errorf("error creating request: %w", err)
-	}
-	defer res.Body.Close()
-
-	data, err := io.ReadAll(res.Body)
-	return data, err
-}
+// // Convert the slice of bytes to a string with the built-in string() type conversion.
+// // Print the string representation of the bytes to the console.
+// // It should look like a big ugly string of text.
 
 
-func main() {
-	items, err := getItemData()
-	if err != nil {
-		log.Fatalf("error getting item data: %v", err)
-	}
+// package main
 
-	// Don't edit above this 
+// import (
+// 	"fmt"
+// 	"io"
+// 	"net/http"
+// 	"log"
+// )
 
-	stringifiedData := string(items)
-	fmt.Println(stringifiedData)
+// func getItemData() ([]byte, error) {
+// 	res, err := http.Get("https://api.boot.dev/v1/courses_rest_api/learn-http/items")
+// 	if err != nil {
+// 		return nil, fmt.Errorf("error creating request: %w", err)
+// 	}
+// 	defer res.Body.Close()
+
+// 	data, err := io.ReadAll(res.Body)
+// 	return data, err
+// }
+
+
+// func main() {
+// 	items, err := getItemData()
+// 	if err != nil {
+// 		log.Fatalf("error getting item data: %v", err)
+// 	}
+
+// 	// Don't edit above this 
+
+// 	stringifiedData := string(items)
+// 	fmt.Println(stringifiedData)
 	
-}
+// }
