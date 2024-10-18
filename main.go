d94e5b3fa19942c29ab4438e2eb1e9de6ffa2501
imhasandl <imhasandl04@gmail.com>
2024-10-18 21:24:27 +0500

    new path handlers

--- main.go
+++ main.go
@@ -3,20 +3,27 @@ package main
 import (
    "log"
 	"net/http"
-)  
-   
+) 
+
 func main(){
 	const filepathRoot = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr: ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving files from %s\n. You are at the port: %s\n", filepathRoot, port)
+	log.Printf("Serving files from %s\n You are at the port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
-}  
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
