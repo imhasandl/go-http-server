2a7ec21d93c60797b00ca2c8cef9a2b6d977e9ae
hasan <imhasandl04@gmail.com>
2024-11-15 04:38:35 +0500

    handlers

--- main.go
+++ main.go
@@ -3,18 +3,34 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+	const filepath = "."
 	const port = "8080"
 
 	mux := http.NewServeMux()
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepath))))
+
+	mux.Handle("/app/", fsHandler)
+	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
-	log.Printf("Serving on port: %s", port)
+	log.Printf("Serving on port: %s and filepath %s", port, filepath)
 	log.Fatal(srv.ListenAndServe())
 }
