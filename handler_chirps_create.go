8ee08c232c046d2e41762675ab793410eb276c85
hasan <imhasandl04@gmail.com>
2024-11-29 21:16:57 +0500

    create chirp containing the user id who created and the body

--- handler_chirps_create.go
+++ handler_chirps_create.go
@@ -15,8 +15,8 @@ type Chirp struct {
 	ID        uuid.UUID `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
-	UserID    uuid.UUID `json:"user_id"`
 	Body      string    `json:"body"`
+	UserID    uuid.UUID `json:"user_id"`
 }
 
 func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request) {
@@ -39,7 +39,10 @@ func (cfg *apiConfig) handlerChirpsCreate(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	chirpID := uuid.New()
+
 	chirp, err := cfg.db.CreateChirp(r.Context(), database.CreateChirpParams{
+		ID:     chirpID,
 		Body:   cleaned,
 		UserID: params.UserID,
 	})
